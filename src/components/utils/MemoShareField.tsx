4394fd3e89e7f547f1ce103dceec9710a1b35d44
ShSato4 <shsato4@ShSato4noMacBook-Air.local>
2022-08-27 11:10:42 +0900

    共有フォーム開発途中。バグあり。。。

--- src/components/utils/MemoShareField.tsx
+++ src/components/utils/MemoShareField.tsx
@@ -1,27 +1,73 @@
 import React, { useState } from 'react'
 import Typography from '@mui/material/Typography'
 import InputLabel from '@mui/material/InputLabel'
-import Input from '@mui/material/Input'
-import FormControl from '@mui/material/FormControl'
+import Box from '@mui/material/Box'
 import Checkbox from '@mui/material/Checkbox'
 import FormGroup from '@mui/material/FormGroup'
 import FormControlLabel from '@mui/material/FormControlLabel'
+import PasswordField from '../utils/PasswordField'
 
-const MemoShareField = () => {
+interface Props {
+  id: string
+  shareCheck: boolean
+  setShareCheck: React.Dispatch<React.SetStateAction<boolean>>
+  aikotoba: string
+  setAikotoba: React.Dispatch<React.SetStateAction<string>>
+  checkAikotoba: string
+  setCheckAikotoba: React.Dispatch<React.SetStateAction<string>>
+}
+
+const MemoShareField: React.FC<Props> = (props) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value)
+    props.setShareCheck(event.target.value === 'on' ? true : false)
+  }
+
   return (
-    <>
+    <Box>
       <FormGroup>
-        <FormControlLabel control={<Checkbox />} label='許可する' />
-      </FormGroup>
-      <FormControl sx={{ margin: 1, width: '100%' }} variant='standard'>
-        <InputLabel htmlFor='standard-adornment-password'>Aikotoba</InputLabel>
-        <Input
-          id='standard-adornment-password'
-          type='text'
-          sx={{ fontSize: 20, fontFamily: 'monospace' }}
+        <FormControlLabel
+          control={
+            <Checkbox value={props.shareCheck} onChange={handleChange} />
+          }
+          label='許可する'
         />
-      </FormControl>
-    </>
+      </FormGroup>
+      {props.shareCheck ? (
+        <>
+          <Typography>
+            他の人にメモを共有する際に使用する合言葉（パスワード）を入力してください。
+          </Typography>
+          <Typography variant='h6' gutterBottom>
+            合言葉 *
+          </Typography>
+          <InputLabel htmlFor='standard-adornment-password'>
+            Password
+          </InputLabel>
+          <PasswordField
+            id={'aikotoba'}
+            text={props.aikotoba}
+            setText={props.setAikotoba}
+            label={'合言葉'}
+            min={8}
+            max={20}
+          />
+          <Typography variant='h6' gutterBottom>
+            合言葉（確認用） *
+          </Typography>
+          <PasswordField
+            id={'check-aikotobae'}
+            text={props.checkAikotoba}
+            setText={props.setCheckAikotoba}
+            label={'合言葉 (確認用)'}
+            min={8}
+            max={20}
+          />
+        </>
+      ) : (
+        ''
+      )}
+    </Box>
   )
 }
 
